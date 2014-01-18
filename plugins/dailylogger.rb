eff4251b47b1a848be5ab31534d25d8f6f7caa52
Ammon Casey <ammon@caseyohana.com>
2014-01-17 15:31:41 -1000

    [dailylogger] added gist to code category

--- plugins/dailylogger.rb
+++ plugins/dailylogger.rb
@@ -49,7 +49,10 @@ config = {
     'lastfm_include_timestamps (true/false) will add a timestamp prefix based on @time_format to each song',
     '***Github***',
     'Logs daily Github activity for the specified user',
-    'github_user should be your Github username'
+    'github_user should be your Github username',
+    '***Gist***',
+    'Logs daily Gists for the specified user',
+    'gist_user should be your Github username'
   ],
   'foursquare_feed' => '',
   'instapaper_feeds' => [],
@@ -73,7 +76,8 @@ config = {
   'lastfm_include_timestamps' => false,
   'lastfm_user' => '',
   'lastfm_feeds' => ['recent', 'loved'],
-  'github_user' => ''
+  'github_user' => '',
+  'gist_user' => ''
 }
 $slog.register_plugin({ 'class' => 'DailyLogger', 'config' => config })
 
@@ -860,6 +864,65 @@ class DailyLogger < Slogger
     @@code_content += entrytext unless entrytext == ''
   end
 
+  # ---------------------------
+  # Gist
+  # ---------------------------
+  def do_gist
+    if @config.key?(self.class.name)
+        config = @config[self.class.name]
+        if !config.key?('gist_user') || config['gist_user'] == ''
+          @log.warn("RSS feeds have not been configured or a feed is invalid, please edit your slogger_config file.")
+          return
+        end
+    else
+      @log.warn("Gist user has not been configured, please edit your slogger_config file.")
+      return
+    end
+    @log.info("Logging gists for #{config['gist_user']}")
+    begin
+      url = URI.parse "https://api.github.com/users/#{config['gist_user']}/gists"
+
+      http = Net::HTTP.new url.host, url.port
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.use_ssl = true
+
+      res = nil
+
+      http.start do |agent|
+        res = agent.get(url.path).read_body
+      end
+    rescue Exception => e
+      raise "ERROR retrieving Gist url: #{url}"
+      p e
+    end
+    # begin
+    #   gist_url = URI.parse("https://api.github.com/users/#{@user}/gists")
+    #   res = Net::HTTPS.get_response(gist_url).body
+
+    return false if res.nil?
+    json = JSON.parse(res)
+
+    output = ""
+    entrytext = ""
+
+    json.each {|gist|
+      date = Time.parse(gist['created_at'])
+      if date > @timespan
+        output += "* Created [Gist ##{gist['id']}](#{gist["html_url"]})\n"
+        output += "    * #{gist["description"]}\n" unless gist["description"].nil?
+      else
+        break
+      end
+    }
+
+    return false if output.strip == ""
+    entry = "## Gists for #{Time.now.strftime(@date_format)}:\n\n#{output}\n#{config['gist_tags']}"
+    if output != ''
+      entrytext = "##### Gists\n" + output
+    end
+    @@code_content += entrytext unless entrytext == ''
+  end
+
 
 
   # ---------------------------
@@ -954,6 +1017,7 @@ class DailyLogger < Slogger
     content = ''
 
     do_github
+    do_gist
 
     if @@code_content != ''
       content += "### Code\n" + @@code_content + "\n\n"
